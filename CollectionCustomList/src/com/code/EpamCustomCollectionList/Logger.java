ca7b293ea2a325a5ef98303bf47c5e5ad3e6777a
Gajula Bullaiah <Gajula Bullaiah@DESKTOP-A5HM1LL>
2020-07-24 20:44:44 +0530

    commit

---
+++ CollectionCustomList/src/com/code/EpamCustomCollectionList/Logger.java
@@ -0,0 +1,10 @@
+package com.code.EpamCustomCollectionList;
+
+public class Logger {
+
+	public static Logger getLogger(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ CollectionCustomList/src/module-info.java
@@ -0,0 +1,2 @@
+module collectionCustomList {
+}
